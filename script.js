4a5a2310575b033892e4033543264af315d20b68
gightkingss <gightkingss@gmail.com>
2023-11-04 01:29:51 +0300

    login updates

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 
-  document.getElementById('toggleReviews').addEventListener('click', function() {
-    var reviewsSection = document.getElementById('reviews');
-    if (reviewsSection.style.display === 'none') {
-        reviewsSection.style.display = 'block';
-    } else {
-        reviewsSection.style.display = 'none';
-    }
-});
-var buttons = document.getElementsByTagName("button");
+//   document.getElementById('toggleReviews').addEventListener('click', function() {
+//     var reviewsSection = document.getElementById('reviews');
+//     if (reviewsSection.style.display === 'none') {
+//         reviewsSection.style.display = 'block';
+//     } else {
+//         reviewsSection.style.display = 'none';
+//     }
+// });
+// var buttons = document.getElementsByTagName("button");
 
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-  });
-}
+// for (var i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//   });
+// }
+//**************deno ***********AFTER SIGING UP */
+function signIn() {
+  alert("Successfully Signed In. Welcome :)");
+  document.getElementById("loginLinks").style.visibility = "hidden";
+  window.location.href = "index.html";
+};
