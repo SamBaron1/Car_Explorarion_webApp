189630a3157a00ae2c5f2ff2e29f9c8f2839be62
Sam Baron <samuelng0001@gmail.com>
2023-11-14 01:06:38 +0300

    css here and there

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    require_once 'database/connection.php';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="fontawesome-free-6.4.2-web/css/all.min.css">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    
     <!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-8Z+Xj1zVJ3+U6vYJf2Ywz5t7K1L9x9h0Q6vJ8yZtXv7HlqLsQO2jKmG9gRb5rZsWxW8yLrBdQq4fVl3vz1+2jA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -18,70 +24,79 @@
 
 <body>
     <div class="welcome-hero">
-    <div class="header">
-        <div id="loginLinks">
+        <div class="header">
+            <div id="loginLinks">
 
-            <div class="links">
-                <a href="login.php" id="login">Login</a>
-                <a href="sign_up.php" id="signUp">Sign Up</a>
-            </div>
-            <div class="navbaricons">
-                <i class="fa-solid fa-bars" id="opennavbtn"></i>
-                <i class="fa-solid fa-xmark" id="closenavbtn" style="display: none;"></i>
-            </div>
-
-        </div>
-
-        <!-- <div id="searchContainer">
-            <input type="text" id="searchInput" placeholder="Search for cars...">
-            <button id="searchButton">Search</button>
-        </div> -->
-    </div>
-    <div class="container">
-        <div class="row">
-            
-            <div class="icon">
-                
-                    <img src="images/images/featured-cars/Bugatti.jpeg" alt="icon">
-            </div>
-     
-        </div>
-        <div class="navbar">
-            <ul class="nav-list">
-                <li><a href="#home">HOME</a></li>
-                <li><a href="#models">FEATURED</a></li>
-                <li><a href="#types">SERVICES</a></li>
-                <li><a href="#clients">CLIENTS</a></li>
-                <li><a href="#logos">BRANDS</a></li>
-                <li><a href="#contacts">CONTACTS</a></li>
-            </ul>
-            
-            <div class="profile">
-                <div class="dashboard">
-                    <div class="profile_pic">
-                        <img src="images/images/featured-cars/alphard.jpeg" alt="">
+                <?php if(!isset($_SESSION['user-email'])):?>
+                    <div class="links">
+                        <a href="login.php" id="login">Login</a>
+                        <a href="sign_up.php" id="signUp">Sign Up</a>
                     </div>
-                        <div class="profile_others">
-                    <ul>
-                        <li id="debug"><a href="dashboard.php">Dashboard</a></li>
-                        <li>Logout</li>
-                    </ul>
-                        </div>
+                <?php endif?>
+                <div class="navbaricons">
+                    <i class="fa-solid fa-bars" id="opennavbtn"></i>
+                    <i class="fa-solid fa-xmark" id="closenavbtn" style="display: none;"></i>
                 </div>
+
+            </div>
+
+            <!--<div id="searchContainer">
+                <input type="text" id="searchInput" placeholder="Search for cars...">
+                <button id="searchButton">Search</button>
+            </div>--> 
+        </div> 
+        
+        <div class="nav-container">
+
+            <div class="navbar">
+                <div class="email">
+                    <p>@Smartcars.com</p>
+                </div>
+                <ul class="nav-list">
+                    <li><a href="#home">HOME</a></li>
+                    <li><a href="#featured">FEATURED</a></li>
+                    <li><a href="#services">SERVICES</a></li>
+                    <li><a href="#clients">CLIENTS</a></li>
+                    <li><a href="#brands">BRANDS</a></li>
+                    <li><a href="#contacts">CONTACTS</a></li>
+                </ul>
+            
+                <?php
+                        if(isset($_SESSION['user-email'])): ?>
+                <div class="profile">
+                    <div class="dashboard">
+                        <div class="profile_pic">
+                            <?php
+                            $image=$_SESSION['user-email'];
+                            $image_query="SELECT profilepic FROM users WHERE email='$image'";
+                            $image_results=mysqli_query($conn,$image_query);
+                            $image_db=mysqli_fetch_assoc($image_results);
+                            $profilepic=$image_db['profilepic'];
+                            echo '<img src="'.$profilepic .'"alt="">';
+                            ?>
+                        </div>
+                            <div class="profile_others">
+                                <ul>
+                                    <li id="debug"><a href="dashboard.php">Dashboard</a></li>
+                                    <li><a href="<?=ROOT_URL.'logout.php' ?>">Logout</a></li>
+                                </ul>
+                            </div>
+                    </div>
+                </div>
+                <?php endif?>
+            </div>
+        </div>
+
+        <div class="home-container" id="home">
+            <div class="welcome-hero-txt">
+                <h2>get your desired car in resonable price</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore   magna aliqua. 
+                </p>
+                <button class="welcome-btn" onclick="window.location.href='#'">contact us</button>
             </div>
         </div>
     </div>
-
-    <div class="home-container" id="home">
-        <div class="welcome-hero-txt">
-            <h2>get your desired car in resonable price</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore   magna aliqua. 
-            </p>
-            <button class="welcome-btn" onclick="window.location.href='#'">contact us</button>
-        </div>
-    </div>
-</div>
     
 
 
@@ -207,10 +222,7 @@
             </div>
         </div>
     </div>
-
-    <div class="body-h1">
-        <h1>Featured CARS...</h1>
-    </div>
+    <h1 class="sub-title">Featured <span>Cars</span></h1>
     <div class="car-details" id="models">
         <div class="car-container">
             <img src="images/images/featured-cars/suzuki.jpeg" alt="Car Image1">
@@ -450,11 +462,9 @@
 
 
 
-    <div class="body-h1">
-        <h1>Our Services</h1>
-    </div>
             <!--service start -->
             <div id="service" class="service">
+            <h1 class="sub-title">Our <span>Services</span></h1>
                 <div class="service-container">
                         
                         <div class="single-service-item">
@@ -519,11 +529,9 @@
                 </div><!--/.container-->
             </div>
 
-            <div class="body-h1">
-                <h1>What Our Clients Say</h1>
-            </div>
     		<!-- clients-say strat -->
             <div class="clients-say" id="clients">
+            <h1 class="sub-title">What Our <span>Clients Say</span></h1>
                     <div class="testimonials">
                         <div class="testimonial-box">
                             <div class="testimonial-img">
@@ -571,38 +579,78 @@
                     </div>
             </div>
 		<!-- clients-say end -->
-        <div class="body-h1">
-            <h1>Our Brands</h1>
-        </div>
+
 		<!--brand strat -->
 		<section id="brand"  class="brand">
+        <h1 class="sub-title">Our <span>Brands</span></h1>
 			<div class="brand-container" id="logos">
 				<div class="brand-area">
 						<div class="item">
 							<a href="#">
 								<img src="images/images/brand/fordlogo.png" alt="brand-image" />
 							</a>
+                            <div class="description">
+                                <p>Ford</p>
+                            </div>
 						</div><!--/.item-->
 						<div class="item">
 							<a href="#">
 								<img src="images/images/brand/benzcars.png" alt="brand-image" />
 							</a>
+                            <div class="description">
+                                <p>Mercedes-Benz</p>
+                            </div>
 						</div><!--/.item-->
 						<div class="item">
 							<a href="#">
 								<img src="images/images/brand/toyotabrand.jpeg" alt="brand-image" />
 							</a>
+                            <div class="description">
+                                <p>Toyota</p>
+                            </div>
 						</div><!--/.item-->
 
 						<div class="item">
 							<a href="#">
 								<img src="images/images/brand/audi.png" alt="brand-image" />
 							</a>
+                            <div class="description">
+                                <p>Audi</p>
+                            </div>
 						</div><!--/.item-->
                         <div class="item">
 							<a href="#">
 								<img src="images/images/brand/mitsu.png" alt="brand-image" />
 							</a>
+                            <div class="description">
+                                <p>Mitsubishi</p>
+                            </div>
+						</div><!--/.item-->
+
+
+                        <div class="item">
+							<a href="#">
+								<img src="images/images/brand/volkswagenpng.png" alt="brand-image" />
+							</a>
+                            <div class="description">
+                                <p>Volkswagen</p>
+                            </div>
+						</div><!--/.item-->
+                        <div class="item">
+							<a href="#">
+								<img src="images/images/brand/landrover.jpeg" alt="brand-image" />
+							</a>
+                            <div class="description">
+                                <p>LandRover</p>
+                            </div>
+						</div><!--/.item-->
+                        <div class="item">
+							<a href="#">
+								<img src="images/images/brand/suzukii.png" alt="brand-image" />
+							</a>
+                            <div class="description">
+                                <p>Suzuki</p>
+                            </div>
 						</div><!--/.item-->
 				</div><!--/.clients-area-->
 
@@ -610,56 +658,60 @@
 
 		</section><!--/brand-->	
 		<!--brand end -->
-        <div class="footer-message">
-            <p>YEAH WE FELL IN LOVE WITH MACHINES!!!!!!</P>
+        <footer>
+            <div class="footer">
+                <div class="grid-container">
+                    <div class="footer-row">
+                        <h3>Get in Touch</h3>
+                        <p><i class="fa fa-phone" aria-hidden="true"></i> +254-7483-742-57</p>
+                        <p><i class="fa fa-envelope" aria-hidden="true"></i> Email Us:smartcars1@gmail.com</p>
+                        <p><i class="fa fa-globe" aria-hidden="true"></i> www.smartcars.com</p>
+                        <p><i class="fa fa-map-marker" aria-hidden="true"></i> Nairobi, Kenya</p>
+                        <p>Follow us:</p>
+                        <div class="social-icons">
+                            <i class="fa-brands fa-whatsapp" aria-hidden="true"></i>
+                            <i class="fa-brands fa-facebook" aria-hidden="true"></i>
+                            <i class="fa-brands fa-twitter" aria-hidden="true"></i>
+                            <i class="fa-brands fa-telegram" aria-hidden="true"></i>
+                            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
+                            <i class="fa-brands fa-youtube" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                
+                    <div class="footer-row2">
+                        <h3>Recent Posts</h3>
+                        <p>Events</p>
+                        <p>Latest Cars</p>
+                        <p>Car Prices</p>
+                        <p>Highest Rated Cars</p>
+                        <div class="final">
+                            we are the bad guys!!!
+                        </div>
+                    
+                    </div>
+                
+                    <div class="footer-row3" id="contact">
+                        <h3>Site Links</h3>
+                        <p>Contact Us</p>
+                        <p>About Us</p>
+                        <p>Directions</p>
+                        <p>Car Wallpapers</p>
+                        <p>Promotions</p>
+                        <p>Blog</p>
+                        <p>FAQs</p>
+                    </div>
+                </div>
+            </div>
+        
+            
+        </footer>
+        <div class="last-text">
+            <p>Developed with much Honour by CS BOYS &copy; 2023. Car Exploration. All Rights Reserved.
+                Terms of service. Privacy policy.
+            </p>
+
         </div>
-    <footer class="grid-container">
-        <div class="footer-row">
-            <h3>Get in Touch</h3>
-            <p><i class="fa fa-phone" aria-hidden="true"></i> +254-7483-742-57</p>
-            <p><i class="fa fa-envelope" aria-hidden="true"></i> Email Us:smartcars1@gmail.com</p>
-            <p><i class="fa fa-globe" aria-hidden="true"></i> www.smartcars.com</p>
-            <p><i class="fa fa-map-marker" aria-hidden="true"></i> Nairobi, Kenya</p>
-            <p>Follow us:</p>
-        </div>
-    
-        <div class="footer-row">
-            <h3>Recent Posts</h3>
-            <p>Events</p>
-            <p>Latest Cars</p>
-            <p>Car Prices</p>
-            <p>Highest Rated Cars</p>
-        </div>
-    
-        <div class="footer-row" id="contact">
-            <h3>Site Links</h3>
-            <p>Contact Us</p>
-            <p>About Us</p>
-            <p>Directions</p>
-            <p>Car Wallpapers</p>
-            <p>Promotions</p>
-            <p>Blog</p>
-            <p>FAQs</p>
-        </div>
-    
-        <div class="social-icons">
-            <i class="fa-brands fa-whatsapp" aria-hidden="true"></i>
-            <i class="fa-brands fa-facebook" aria-hidden="true"></i>
-            <i class="fa-brands fa-twitter" aria-hidden="true"></i>
-            <i class="fa-brands fa-telegram" aria-hidden="true"></i>
-            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
-            <i class="fa-brands fa-youtube-play" aria-hidden="true"></i>
-        </div>
-    
-        <div class="copyright">
-            <p> Copyright &copy; 2023 Car Exploration </p>
-            <p> All Rights Reserved </p>
-            <p> Terms of service </p>
-            <p>Privacy policy<p></p>
-            <p> Contact Us </p>
-        </div>
-    </footer>
-    
+        <a href="#" class="top"><i class="bx bx-up-arrow-alt"></i> </a>
     
 </body>
 
