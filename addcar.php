bd487dc15c5f0481a97bd7129e665323e90d1bc8
Brian Kariuki <kariukibrian087@gmail.com>
2023-11-23 20:01:22 +0300

    Added addcar backend

--- addcar.php
+++ addcar.php
@@ -1,5 +1,16 @@
 <?php 
     require_once 'database/connection.php';
+
+    if(isset($_SESSION['addcardata'])){
+    $manufactureyear=$_SESSION['addcardata']['manufactureyear'] ?? null;
+    $make=$_SESSION['addcardata']['make'] ?? null;
+    $model=$_SESSION['addcardata']['model'] ?? null;
+    $bodytype=$_SESSION['addcardata']['bodytype'] ?? null;
+    $carcondition=$_SESSION['addcardata']['carcondition'] ?? null;
+    $price=$_SESSION['addcardata']['price'] ?? null;
+    $country=$_SESSION['addcardata']['country'] ?? null;
+    $mileage=$_SESSION['addcardata']['mileage'] ?? null;
+    }
 ?>
 <?php
  if(!isset($_SESSION['user-email'])){
@@ -18,7 +29,7 @@
             background: url(Images/fc4.png);
             background-repeat: no-repeat;
             background-size: 100vw 100vh;
-            height: 100vh;
+            height: fit-content;
             display: flex;
             align-items: center;
             justify-content: center;
@@ -57,51 +68,77 @@
             background-color: red;
         }
 
+        .errormessage{
+    background: red;
+    padding: 5px;
+    border-radius: 8px;
+}
+
     </style>
 </head>
 <body class="dashboardbody">
-    <form action="">
+    <form action="<?=ROOT_URL?>addcarback.php" method="post" enctype="multipart/form-data">
         <table class="dashboard">
             <th>ADD CAR</th>
+            <?php
+        if(isset($_SESSION['addcar'])): ?>
+        <div class="errormessage">
+            <p><?=$_SESSION['addcar'];
+            unset($_SESSION['addcar']);
+            ?></p>
+        </div>
+        <?php endif ?>
             <tr>
                 <td>Input year:</td>
-                <td><input type="text" value="2023"></td>
+                <td><input type="text" placeholder="2023" name="manufactureyear" value="<?=$manufactureyear?>"></td>
             </tr>
             <tr>
                 <td>Input make:</td>
-                <td><input type="text" placeholder="eg Makedez benz"></td>
+                <td><input type="text" placeholder="eg Makedez benz" name="make" value="<?=$make?>"></td>
             </tr>
             <tr>
                 <td>Input model:</td>
-                <td><input type="text" placeholder="eg Subaru"></td>
+                <td><input type="text" placeholder="eg Subaru" name="model" value="<?=$model?>"></td>
             </tr>
             <tr>
                 <td>Input bodystyle:</td>
-                <td><input type="text" placeholder="Sedan"></td>
+                <td><input type="text" placeholder="Sedan" name="bodytype" value="<?=$bodytype?>"></td>
             </tr>
             <tr>
                 <td>Input Car condition:</td>
-                <td><input type="text" placeholder="brand new"></td>
+                <td><input type="text" placeholder="brand new" name="carcondition" value="<?=$carcondition?>"></td>
             </tr>
             <tr>
                 <td>Input Price:</td>
-                <td><input type="text" placeholder="20000"></td>
+                <td><input type="text" placeholder="20000" name="price" value="<?=$price?>"></td>
             </tr>
             <tr>
                 <td>Country of origin:</td>
-                <td><input type="text" placeholder="Kenya"></td>
+                <td><input type="text" placeholder="Kenya" name="country" value="<?=$country?>"></td>
             </tr>
             <tr>
                 <td>Mileage:</td>
-                <td><input type="text" placeholder="20000"></td>
+                <td><input type="text" placeholder="20000" name="mileage" value="<?=$mileage?>"></td>
             </tr>
             <tr>
-                <td>Images:</td>
-                <td><input type="file"></td>
+                <td>Front view:</td>
+                <td><input type="file" name="front"></td>
+            </tr>
+            <tr>
+                <td>Back view:</td>
+                <td><input type="file" name="back"></td>
+            </tr>
+            <tr>
+                <td>Sideview:</td>
+                <td><input type="file" name="side"></td>
+            </tr>
+            <tr>
+                <td>Interior:</td>
+                <td><input type="file" name="interior"></td>
             </tr>
             <tr>
                 <td>
-                    <input type="submit" id="submit">
+                    <input type="submit" id="submit" name="submit">
                 </td>
             </tr>
         </table>
---
+++ make.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="other.css">
+</head>
+<body>
+    <div class="makecontainer">
+        <div class="makecontent">
+            <img src="images/images/brand/suzuki.png" alt="">
+            <h1>Suzuki</h1>
+            <p>Born in Japan, Suzuki is an automaker that has made a name for itself by crafting vehicles that emphasize value and affordability. Through the years, the brand's lineup has included sedans, wagons and SUVs. However, in late 2012 the company decided to stop selling new vehicles in the United States.</p>
+            <div class="makecars">
+                <hr><h1>Suzuki cars</h1>
+                <div class="makecarsimages">
+                    <img src="images/fc1.png" alt="">
+                    <img src="images/fc1.png" alt="">
+                    <img src="images/fc1.png" alt="">
+                    <img src="images/fc1.png" alt="">
+                    <img src="images/fc1.png" alt="">
+                    <img src="images/fc1.png" alt="">
+                    <img src="images/fc1.png" alt="">
+                    <img src="images/fc1.png" alt="">
+                    <img src="images/fc1.png" alt="">
+                    <img src="images/fc1.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
